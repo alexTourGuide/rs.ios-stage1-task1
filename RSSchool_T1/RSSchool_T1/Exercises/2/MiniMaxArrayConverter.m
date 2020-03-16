a2c1b0333c38163bc65b4dc4cb008be811ec84ad
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-16 19:34:42 +0300

    feat: implement miniMaxConverter-function

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -2,9 +2,27 @@
 
 @implementation MiniMaxArrayConverter
 
-// Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    NSMutableArray <NSNumber*>* fiveSums = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < array.count; i++) {
+        NSMutableArray *fourNumb = [[NSMutableArray alloc] initWithArray:array];
+        [fourNumb removeObjectAtIndex:i];
+
+        int sumOfFour = 0;
+        for (NSUInteger i = 0; i < fourNumb.count; i++) {
+            int element = [[fourNumb objectAtIndex:i] intValue];
+            sumOfFour += element;
+        }
+        NSNumber* sum = [[NSNumber alloc] initWithInt:sumOfFour];
+        [fiveSums addObject:sum];
+    }
+    NSNumber* max = [fiveSums valueForKeyPath:@"@max.intValue"];
+    NSNumber* min = [fiveSums valueForKeyPath:@"@min.intValue"];
+    NSArray <NSNumber*>* miniMaxArray = [[NSArray alloc] initWithObjects:min, max, nil];
+     
+    return miniMaxArray;
 }
 
 @end
