7eb7b34f24fdd1122efbdc5315f3d218e1d49be6
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-16 19:40:34 +0300

    feat: implement stringParser-function

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -2,9 +2,42 @@
 
 @implementation Parser
 
-// Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    unichar beginBrackets[] = { '<', '(', '[' };
+    unichar endBrackets[] = { '>', ')', ']' };
+    NSMutableArray <NSString*> *finalArray = [[NSMutableArray alloc] init];
+    
+    NSUInteger len = string.length;
+    unichar buffer[len + 1];
+    [string getCharacters:buffer range:NSMakeRange(0, len)];
+    NSMutableOrderedSet<NSValue *> *results = [NSMutableOrderedSet orderedSet];
+    NSInteger depth = 0;
+    
+    for (int string = 0; string < len; string++){
+        for(int bracket = 0; bracket < 3; bracket++){
+            if (buffer[string] == beginBrackets[bracket]){
+                for (int i = string + 1; i < len ; i++) {
+                    if (buffer[i] == beginBrackets[bracket]) {
+                        depth++; continue;
+                    }
+                    if (buffer[i] == endBrackets[bracket] && depth != 0) {
+                        depth--; continue;
+                    }
+                    if (buffer[i] == endBrackets[bracket] && depth == 0) {
+                        [results addObject:[NSValue valueWithRange:NSMakeRange(string + 1, i - string - 1)]]; break;
+                        }
+                    }
+                }
+            }
+        }
+
+    for (NSUInteger i = 0; i < [results count]; i++) {
+        NSValue *value = [results objectAtIndex:i];
+        NSRange range = [value rangeValue];
+        NSString *subString = [string substringWithRange:range];
+        [finalArray addObject:subString];
+    }
+    return finalArray;
 }
 
 @end
