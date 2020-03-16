ede5d80bff521ab0773a64e054457f6a77afd490
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-16 19:38:11 +0300

    feat: implement billCounter-function

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,20 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    NSMutableArray *twoEated = [[NSMutableArray alloc] initWithArray:bill];
+    [twoEated removeObjectAtIndex:(NSUInteger)index];
+    NSNumber *sumOnlyTwo = [twoEated valueForKeyPath:@"@sum.self"];
+    int sumOnTwo = [sumOnlyTwo intValue] / 2;
+    int giveSum = [sum intValue];
+    if (giveSum <= sumOnTwo) {
+        NSString *output = @"Bon Appetit";
+        return output;
+    } else {
+        int shouldBePaid = giveSum - sumOnTwo;
+        NSString *output = [NSString stringWithFormat:@"%d", shouldBePaid];
+        return output;
+    }
 }
 
 @end
