24a6b994fdce0abc085bfcfe9253b8983fc4516a
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-16 19:15:06 +0300

    feat: implement convertToHappy-function

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -2,9 +2,34 @@
 
 @implementation T1Array
 
-// Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+        if (sadArray.count == 0) {
+            NSArray *happyArray = [[NSArray alloc] init];
+            return happyArray;
+        } else {
+    
+        NSMutableArray *happyArray = [[NSMutableArray alloc] init];
+        [happyArray addObject: [sadArray firstObject]];
+            for (NSUInteger i = 1; i < sadArray.count-1; i++) {
+                int left = [[sadArray objectAtIndex:i-1] intValue];
+                int central = [[sadArray objectAtIndex:i] intValue];
+                int right = [[sadArray objectAtIndex:i+1] intValue];
+                    if (central < (left + right)) {
+                        [happyArray addObject: [sadArray objectAtIndex:i]];
+                        for (NSUInteger i = 1; i < happyArray.count-1; i++) {
+                            int left = [[happyArray objectAtIndex:i-1] intValue];
+                            int central = [[happyArray objectAtIndex:i] intValue];
+                            int right = [[happyArray objectAtIndex:i+1] intValue];
+                            if (central > (left + right)) {
+                                [happyArray removeObjectAtIndex:i];
+                }
+            }
+        }
+    }
+            [happyArray addObject: [sadArray lastObject]];
+            NSArray *happyArray_final = [[NSArray alloc] initWithArray:happyArray];
+            return happyArray_final;
+    }
 }
-
-@end
+    @end
+    
