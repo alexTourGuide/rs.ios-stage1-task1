f02bda5ab4058f0dcebea51d18b9e299bc1ee4e4
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-03-16 19:44:15 +0300

    feat: implement highestPalindrom-function

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -2,9 +2,46 @@
 
 @implementation PalindromeSolver
 
-// Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    
+    NSMutableArray *tempDataArray = [[NSMutableArray alloc] init];
+    NSInteger numberOfDigits = [n integerValue];
+    NSInteger maxAllowedChanges = [k integerValue];
+    NSInteger maxValue = 0;
+    NSInteger factChanges = 0;
+  
+    [s enumerateSubstringsInRange:(NSRange){0, [s length]}
+                          options: NSStringEnumerationByComposedCharacterSequences
+                       usingBlock:^(NSString * _Nullable substring, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
+        [tempDataArray addObject:substring];
+    }];
+    
+    for (long leftDigit = 0, rightDigit = numberOfDigits-1; leftDigit < numberOfDigits/2; leftDigit++, rightDigit--) {
+        NSInteger valueofLeftDidit = [[tempDataArray objectAtIndex:leftDigit] integerValue];
+        NSInteger valueOfRightDigit = [[tempDataArray objectAtIndex:rightDigit] integerValue];
+        NSInteger biggerValue = MAX(valueofLeftDidit, valueOfRightDigit);
+        if (valueofLeftDidit != valueOfRightDigit) {
+            if (maxValue < biggerValue) {
+                maxValue = biggerValue;
+            }
+                factChanges++;
+        }
+    }
+        
+    if (factChanges > maxAllowedChanges) {
+        return @"-1";
+    }
+    
+    for (long left = 0, right = numberOfDigits-1; left < numberOfDigits/2; left++, right--) {
+        if (([[tempDataArray objectAtIndex:left] integerValue] != [[tempDataArray objectAtIndex:right] integerValue]) && maxAllowedChanges > 0) {
+            tempDataArray[right] = [NSNumber numberWithInteger:maxValue];
+            tempDataArray[left] = [NSNumber numberWithInteger:maxValue];
+            maxAllowedChanges --;
+        }
+    }
+    
+    NSString *highestValuePalindrome = [tempDataArray componentsJoinedByString:@""];
+    return highestValuePalindrome;
 }
 
 @end
